30ff1db374c7b215491d5a9b500bae9e7d008fb8
Tejas Nikam <Tejasnikam4515@gmail.com>
2023-02-05 11:37:20 +0530

    Add components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from  "react";
+import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
-      
+      <ExpenseItem/>
     </div>
   );
 }
